3a3a48bf97f90a83ce633803cd6d79cc08ff0f01
KenFordev <saran.warin1998@gmail.com>
2022-12-07 14:22:46 +0700

    First-Project

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-ECHO is on.
+let hambergermenu = document.querySelector('.hamberger__menu')
+
+hambergermenu.addEventListener("click", function(){
+    hambergermenu.classList.toggle("change");
+    let navRightOpen = document.querySelector('.nav-right')
+    navRightOpen.classList.toggle("open__nav")
+})
+
+
